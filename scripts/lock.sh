0bfeca8f468325ca2acc0b3042e3ddaca31d3357
Zakaria Fadli <zakaria.fadli@netatmo.com>
2019-06-12 13:02:01 +0200

    blur lock screen

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -24,8 +24,8 @@ SCREENSHOT="scrot $IMAGE" # 0.46s
 #BLURTYPE="0x5" # 7.52s
 #BLURTYPE="0x2" # 4.39s
 #BLURTYPE="5x2" # 3.80s
-BLURTYPE="2x8" # 2.90s
-#BLURTYPE="2x3" # 2.92s
+# BLURTYPE="2x8" # 2.90s
+BLURTYPE="5x4" # 2.92s
 
 # Get the screenshot, add the blur and lock the screen with it
 $SCREENSHOT
