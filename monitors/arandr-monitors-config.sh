ae03a434dfabe7a6266bd53c032670fa04dd2451
Zakaria Fadli <zakaria.fadli@netatmo.com>
2019-06-28 16:45:04 +0200

    arandr: add monitos work config

---
+++ monitors/arandr-monitors-config.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-3 --off --output HDMI-2 --off --output HDMI-1 --off --output DP-3 --off --output DP-2 --mode 1920x1080 --pos 1920x0 --rotate left --output DP-1 --mode 1920x1080 --pos 0x544 --rotate normal
