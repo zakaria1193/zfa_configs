e5546560a9989b4641591dab9e472b325fa2ae2e
Zakaria Fadli <zakaria.fadli@netatmo.com>
2019-11-26 14:54:51 +0100

    fix raspberry ssh

--- shell/remote_aliases.sh
+++ shell/remote_aliases.sh
@@ -7,5 +7,5 @@ alias     ssh_pi_rem="ssh -l pi     proxy51.rt3.io -p 38390"
 alias    remotes_rem="sensible-browser https://app.remote.it/\#"
 
 alias ssh_laptop_local="ssh -l zfadli $IP_LAPTOP_LOCAL"
-alias ssh_pi_local="ssh -l zfadli $IP_RASPBERRY_LOCAL"
+alias ssh_pi_local="ssh -l pi $IP_RASPBERRY_LOCAL"
 alias ssh_home_pi="ssh -l pi $IP_PUBLIC_HOME"
