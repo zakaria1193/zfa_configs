3508996d7c2227b20745ef0f12464f5468db5857
Zakaria Fadli <zakaria.fadli@netatmo.com>
2019-12-23 05:09:04 +0100

    lundi 23 décembre 2019, 05:09:04 (UTC+0100)

--- shell/git_aliases.sh
+++ shell/git_aliases.sh
@@ -157,7 +157,9 @@ alias tsrc_perso_perso_init="tscr_pull_cfg $MY_REPOS perso"
 function tsrc_push_perso_perso
 {
   tsrc_perso_perso_init
+  tsrc foreach -- git stash
   tsrc foreach -- git pull origin master
+  tsrc foreach -- git stash apply
   tsrc foreach -- git add -A
   tsrc foreach -- git commit -m "$(date)"
   tsrc foreach -- git push
