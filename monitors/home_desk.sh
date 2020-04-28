39e5d4147b6b0ad52a0c5fc6db001a854f1b8827
Zakaria Fadli <zakaria.fadli@netatmo.com>
2020-04-28 03:25:52 +0200

    monitor config scripts

---
+++ monitors/home_desk.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI2 --off --output HDMI1 --off --output DP1 --mode 1920x1080 --pos 1280x0 --rotate normal --output eDP1 --primary --mode 1280x720 --pos 0x360 --rotate normal --output VIRTUAL1 --off
