4579490b1f5e3acdd3b55b302eb814fe50c2259c
Zakaria Fadli <zakaria.fadli@legrand.com>
2022-11-16 16:15:43 +0100

    i3 work config: Generate i3 config

--- i3/work/work_specific_settings.sh
+++ i3/work/work_specific_settings.sh
@@ -1,14 +1,9 @@
 for_window [class="meld"] fullscreen enable
-#for_window [class="^(?i)google-chrome"] border pixel 3
 
-assign [class="Wireshark"] → $workspace8
-assign [class="Nautilus"] → $workspace9
-assign [class="Filezilla"] → $workspace9
-
-for_window [title="Microsoft Teams Notification"] floating enable
+assign [class="Wireshark"] → workspace number 8
+assign [class="Nautilus"] → workspace number 9
 
 exec_always xautolock -time 20 -locker fuzzy_lock -notify 20 -notifier 'xset dpms force off && $LOCK_SCRIPT' &
-
 
 bindsym $mod+F1  exec playerctl play
 bindsym $mod+F2  exec playerctl pause
