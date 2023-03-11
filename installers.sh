cfe2c1623dd6f460a8547b631cc7cbf30aecf281
Zakaria Fadli <zakaria.fadli@legrand.com>
2023-03-11 20:43:36 +0100

    installers: i3 installation shall use latest version to have i3-gaps merged

--- installers.sh
+++ installers.sh
@@ -108,6 +108,14 @@ function install_udev_rules
 ################################################################################
 function install_i3()
 {
+  /usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2023.02.18_all.deb keyring.deb SHA256:a511ac5f10cd811f8a4ca44d665f2fa1add7a9f09bef238cdfad8461f5239cc4
+  sudo apt install ./keyring.deb
+  echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" | sudo tee /etc/apt/sources.list.d/sur5r-i3.list
+  sudo apt update
+  sudo apt install i3
+}
+function install_window_manager()
+{
   # if is_installed i3; then
   #   echo i3 installed
   #   return
@@ -387,7 +395,7 @@ function main()
     install_vscode
     install_git
     install_tools
-    install_i3
+    install_window_manager
     install_udev_rules
     install_terminal_emul
     install_wireshark
