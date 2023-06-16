85a7391fa8ce215062e31086d470214128c1a4ee
Zakaria Fadli <zakaria.fadli@legrand.com>
2023-06-16 18:34:54 +0200

    🔧 chore(common_i3_config.sh): comment out i3blocks status_command and uncomment i3status status_command in bar section to use default i3 status bar

--- i3/common/common_i3_config.sh
+++ i3/common/common_i3_config.sh
@@ -191,9 +191,9 @@ exec_always  --no-startup-id  compton # compton is needed for transparency
 #enable numpad
 exec  --no-startup-id setxkbmap -option keypad:pointerkeys
 bar {
-  # status_command i3status
+  status_command i3status
   # what i prefer to use is the non defauilt i3ibar that is i3blocks -> update submodules of my config file
-  status_command SCRIPT_DIR=$I3BLOCKS_SCRIPTS_DIR i3blocks
+  # status_command SCRIPT_DIR=$I3BLOCKS_SCRIPTS_DIR i3blocks
   # status_command 2>/tmp/i3blocks.err SCRIPT_DIR=$I3BLOCKS_SCRIPTS_DIR  /usr/bin/i3blocks -vvv | tee /tmp/i3blocks.out
   font pango:System San Fransisco Display 7.5
 
