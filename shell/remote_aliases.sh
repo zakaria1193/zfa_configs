3b9b3f36ad7b812afa5caa79a53f76d1f46e8083
Zakaria Fadli <zakaria.fadli@legrand.com>
2024-07-23 09:43:17 +0200

    🔧 fix(remote_aliases.sh): update remote aliases to include wake-on-LAN commands ✨ feat(remote_aliases.sh): add wake-on-LAN aliases for local and remote desktops

--- shell/remote_aliases.sh
+++ shell/remote_aliases.sh
@@ -1,6 +1,10 @@
 IP_DESK_LOCAL="192.168.0.10"
+MAC_DESK="54:bf:64:99:d3:ee"
 IP_PI_LOCAL="192.168.0.11"
-IP_PUBLIC_HOME="93.5.181.114"
+IP_PUBLIC_HOME="ssh.zakariafadli.com"
 
 alias ssh_desk_local="ssh -l zfadli $IP_DESK_LOCAL"
-alias ssh_desk_rem="ssh -l zfadli $IP_PUBLIC_HOME"
+alias ssh_desk_rem="ssh -l zfadli -p 2222 $IP_PUBLIC_HOME"
+alias wake_desk_local="wakeonlan $MAC_DESK"
+alias wake_desk_rem="wakeonlan -i $IP_PUBLIC_HOME $MAC_DESK"
+
