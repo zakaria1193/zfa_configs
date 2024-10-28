9957923658fb9541120b207ed19557cb78333ce1
Zakaria Fadli <zakaria.fadli@legrand.com>
2024-10-29 00:16:16 +0100

    🔧 fix(remote_aliases.sh): update IP_PUBLIC_HOME alias to point to correct domain name for remote access

--- shell/remote_aliases.sh
+++ shell/remote_aliases.sh
@@ -1,7 +1,7 @@
 IP_DESK_LOCAL="192.168.0.10"
 MAC_DESK="54:bf:64:99:d3:ee"
 IP_PI_LOCAL="192.168.0.11"
-IP_PUBLIC_HOME="ssh.zakariafadli.com"
+IP_PUBLIC_HOME="ip.zakariafadli.com"
 
 alias ssh_desk_local="ssh -l zfadli $IP_DESK_LOCAL"
 alias ssh_desk_rem="ssh -l zfadli -p 2222 $IP_PUBLIC_HOME"
