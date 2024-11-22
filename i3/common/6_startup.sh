4841532b324025e6f9f17fd25cf74bd8c7e63946
Zakaria Fadli <zakaria.fadli@legrand.com>
2024-11-22 23:19:16 +0100

    🚀 feat(6_startup.sh): add compton to startup script for enabling window compositing and effects

--- i3/common/6_startup.sh
+++ i3/common/6_startup.sh
@@ -7,6 +7,7 @@ exec --no-startup-id i3-msg "workspace 1; layout tabbed"
 # Start applications
 exec_always --no-startup-id yes | $WALLPAPER_SCRIPT
 exec_always --no-startup-id 1password --silent
+exec_always --no-startup-id compton
 exec_always --no-startup-id xautolock -time 20 -locker fuzzy_lock -notify 20 -notifier '$LOCK_SCRIPT' &
 exec_always --no-startup-id nm-applet blueman-applet
 # exec_always --no-startup-id compton -b # compton is needed for transparency
